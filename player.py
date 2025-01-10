2418f46be5923034fcdd90ba16109861829dadfe
Myra Ilieva <159176371+miraIllieva@users.noreply.github.com>
2025-01-09 23:55:45 -0500

    Add files via upload

---
+++ player.py
@@ -0,0 +1,21 @@
+class Player:
+    def __init__(self, name: str, hp: int, mp: int) -> None:
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills: dict[str, int] = {}
+        self.guild = "Unaffiliated"
+
+    def add_skill(self, skill_name: str, mana_cost: int) -> str:
+        if skill_name in self.skills:
+            return "Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self) -> str:
+        result = (f"Name: {self.name}\nGuild: "
+                  f"{self.guild}\nHP: {self.hp}\n"
+                  f"MP: {self.mp}\n")
+        formatted_skills = [f"==={skill} - {mana}" for skill, mana in self.skills.items()]
+        result += "\n".join(formatted_skills)
+        return result
